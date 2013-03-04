1b0b018655701ce9a3352b661a54dfba7a5b43d2
Garret Alfert <alfert@wevelop.de>
2013-03-04 18:12:47 +0100

    Use FastClick to reduce click delays on touch devices

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-window.Todos = Ember.Application.create();
+window.Todos = Ember.Application.create({
+  ready: function() {
+    window.addEventListener('load', function() {
+      new FastClick(document.body);
+    }, false);
+  }
+});
